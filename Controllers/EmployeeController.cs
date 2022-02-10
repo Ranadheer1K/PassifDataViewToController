c21317e6bc79ac743b4d1bde346defa501395735
ranadheer.k.2451@gmail.com <ranadheer.k.2451@gmail.com>
2022-02-10 15:09:35 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassifDataViewToController.Models;
+
+namespace PassifDataViewToController.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(EmployeeModel collection)
+        {
+            int id = collection.EmpId;
+            string name = collection.EmpName;
+            double salary = collection.EmpSalary;
+
+            return View();
+        }
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PassifDataViewToController.Models
+{
+    public class EmployeeModel
+    {
+        public int EmpId { get; set; }
+        public string? EmpName { get; set; }
+        public double EmpSalary { get; set; }
+    }
+}
